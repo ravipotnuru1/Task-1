af3cd091b31270105f2838d9136268f0b9423d1d
ravipotnuru1 <potnururavi@thestackly.com>
2026-05-16 19:19:16 +0530

    Create Task1.js

---
+++ Task1.js
@@ -0,0 +1,320 @@
+
+// 1. Create a variable using var and store your name.
+var name = "Ravi"
+
+// 2. Print your name using console.log().
+console.log(name)
+
+
+// 3. Create a variable using let and store your age.
+let age = 23
+
+
+// 4. Change the value of age and print it.
+age = 22
+console.log(age)
+
+
+// 5. Create a constant variable for country name.
+const country = "India"
+
+
+// 6. Print the country name.
+console.log(country)
+
+
+// 7. Try redeclaring var variable and observe output.
+var name ="Naveen"
+console.log(name)       // Yes it is possible to Redeclare var variaable.
+
+// 8. Try redeclaring let variable and observe error.
+
+//let age = 25           // No its not possible to redeclare the let variable
+                         // The error is :- "Identifier 'age' has already been declared " 
+
+
+// 9. Try reassigning const variable and observe error.
+
+//const country = "US"     // It is not possible to reassign const variable 
+                        // The error is :- "Identifier 'country' has already been declared"
+
+
+// 10. Write difference between var and let.
+
+// The var variable can be redeclared where as let variable can't be redeclared.
+//  And the scope of the var variable is with in the function but the scope of the let variable is limited to block. 
+
+
+// 11. Write difference between let and const.
+
+// The let variable can be reinitialize or reuse but whereas const varaible cant be reintialize or reuse.
+
+
+// 12. Predict output
+
+var a = 10
+a = 20
+var a = 30
+
+console.log(a)
+
+// The output is "30" 
+
+
+// 13. Predict output
+
+let b = 50
+b = 60
+
+console.log(b)
+
+// The output is "60"
+
+
+// 14. Predict output
+
+const c = 100
+
+console.log(c)
+
+// The ouput is "100"
+
+// 15. Use console.log() to print "Hello World".
+
+console.log("Hello World")
+
+
+// 16. Use alert() to show "Welcome User".
+
+alert("Welcome User")
+
+
+// 17. Use confirm() to ask user:
+// "Do you like JavaScript?"
+
+confirm("Do you like JavaScript")
+
+// 18. Use prompt() to ask user name.
+
+prompt("Enter your name")
+
+// 19. Print prompt value in console.
+
+var userName = prompt("Enter your name")
+console.log(userName)
+
+
+
+// 20. Use document.writeln() to print "Batch 33".
+
+document.writeln("Batch 33")
+
+// 21. Create variable studentName and store your name.
+
+var studentName = "Ravi"
+
+// 22. Create variable studentAge and store your age.
+
+var studentAge = 25
+
+// 23. Print both variables together.
+
+console.log(studentName,studentAge)
+
+// 24. Create a variable for mobile number.
+
+var mobile= 9505133423
+
+
+// 25. Print mobile number using console.log().
+
+console.log(mobile)
+
+
+// 26. Create variable using var for favorite color.
+
+var color = "Red"
+
+
+// 27. Reassign another color value.
+
+color="black"
+
+
+// 28. Create variable using let for marks.
+
+let marks = 95
+
+// 29. Change marks value and print it.
+
+marks = 97
+console.log(marks)
+
+
+// 30. Create const variable for PI value.
+
+const PI=3.14
+
+
+// 31. Print PI value.
+
+console.log(PI)
+
+// 32. Predict output
+
+var x = 5
+var x = 15
+
+console.log(x)
+
+// The output is "15"
+
+// 33. Predict output
+
+let y = 25
+y = 35
+
+console.log(y)
+
+// The output is "35"
+
+// 34. Find error
+
+const z = 40
+//z = 45
+
+console.log(z)
+
+// The const variable cant be reinitialize bcoz constant variable cant be changed within the block.
+// The error is : Assignment to constant variable.
+
+// 35. Create alert box with your college name.
+
+alert("AITAM")
+
+
+// 36. Ask user age using prompt().
+
+var UserAge = prompt(" Enter your Age ")
+
+
+// 37. Print user age in console.
+
+console.log(UserAge)
+// 38. Ask user confirmation:
+// "Did you complete HTML?"
+
+var conf = confirm("Did you complete HTML?")
+
+
+// 39. Print confirm result in console.
+console.log(conf)
+
+
+// 40. Print "JavaScript Class" using document.writeln().
+
+document.writeln(" JavaScript Class")
+
+
+// 41. Create 3 variables:
+// city, state, country.
+
+var city= "Hyderabad"
+var state= "Telengana"
+var country1="india"
+
+// 42. Print all variables together.
+console.log(city,state,country1)
+
+// 43. Create one example for var.
+
+var x = "Var"
+x=" Var Key"
+
+var x = "Var Keyword"
+
+console.log(x)
+
+
+// 44. Create one example for let.
+
+let m = "let"
+m="let keyword"
+
+console.log(m)
+
+// 45. Create one example for const.
+
+const c1 = "const keyword" 
+
+console.log(c1)
+
+// 46. Predict output
+
+//var num = 100
+num = 200
+
+console.log(num)
+
+
+// 47. Predict output
+
+let price = 500
+price = 700
+
+console.log(price)
+
+// Output is "700"
+
+// 48. Predict output
+
+const password = 12345
+
+console.log(password)
+
+// output is "12345"
+
+// 49. Write rules of var.
+
+// Rules of var
+// 1. Declaration:
+        // Declaration is creating or naming a  var variable(Memory).
+        // var p
+
+// 2. Initialization:
+        // Intialization is assigning data to the declared var variable.
+        // p = 10
+
+// 3. Declaration + Initialization
+        // Declaration and initialization of var variable can be done at a time in single line.
+        // var p=10
+
+// 4. Reintialization
+        // we can reintialize the var variable to update the data.
+        // p =100 --> updates the value
+
+// 5. ReDeclaration
+        // We can Redeclare the var variable to update the data.
+        // var p = 200 ----> updates the value.
+
+// 50. Write rules of let and const.
+
+// 1. Declaration:
+        // Declaration is creating or naming a  let variable(Memory).
+        // let k
+
+// 2. Initialization:
+        // Intialization is assigning data to the declared let variable.
+        // k=40
+
+// 3. Declaration + Initialization
+        // Declaration and initialization of let variable can be done at a time in single line.
+        // let k = 60
+
+// 4. Reintialization
+        // we can reintialize the let variable to update the data.
+        // k = 600
+
+// 5. ReDeclaration
+        // We can't Redeclare the var variable to update the data.
+        // let k = 700
